8457345456b7cb8d05155106d6786b23e7818c2a
PBenavides <fob534154@gmail.com>
2021-12-10 09:16:04 -0500

    stored procedures added

--- sql/PROC_WEB.sql
+++ sql/PROC_WEB.sql
@@ -39,6 +39,53 @@ create or replace procedure update_jurado(
     where cod_jurado=cod;
     end;
 
+--procedimiento actualizar compañia
+create or replace procedure update_cia(
+    logo_ compañia.logo%type,
+    cdescripcion_ compañia.cdescripcion%type,
+    nombre_cia_ compañia.nombre_cia%type,
+    colegio_proc_ compañia.colegio_proc%type,
+    fase_alcanzada_  compañia.fase_alcanzada%type,
+    cod_cia_ compañia.cod_cia%type)
+ is
+    begin
+    update compañia set
+    logo = logo_,
+    cdescripcion = cdescripcion_,
+    nombre_cia = nombre_cia_,
+    colegio_proc = colegio_proc_,
+    fase_alcanzada = fase_alcanzada_
+    where cod_cia= cod_cia_;
+    end;
+
+--procedimiento actualizar red social
+create or replace procedure update_red_social(
+    link_  red_social.link%type,
+    rsnombre_  red_social.rsnombre%type,
+    cod_cia_  red_social.cod_cia%type)
+    is
+    begin
+    update red_social set
+    link = link_,
+    rsnombre = rsnombre_
+    where cod_cia = cod_cia_;
+    end;
+
+--procedimiento actualizar tabla participa_en
+
+create or replace procedure update_participa_en(
+    punt_acum_  participa_en.punt_acum%type,
+    cod_fase_  participa_en.cod_fase%type,
+    cod_cia_  participa_en.cod_cia%type)
+    is
+    begin
+    update participa_en set
+    punt_acum = punt_acum_,
+    cod_fase = cod_fase_
+    where cod_cia = cod_cia_;
+    end;
+
+
 --Procedimiento para generar ganadores por ronda, recibe como parametro la cantidad de ganadores a los que se le actualizra su fase alcanzada
 CREATE OR REPLACE PROCEDURE ganadoresRonda(p_cantidad_participantes number) is
     BEGIN
@@ -77,6 +124,8 @@ BEGIN
     DELETE COMPAÑIA WHERE COD_CIA=COD;
 END;
 
+---------------- Dado que cx_Oracle no permite llamar a través de dbms_output, los SELECTS han sido inyectados
+---------------- directamente a través del código SQL. A continuación se verán los selects usados en toda la app.
 --PROMEDIO DE PUNTAJES POR REGION.
 SELECT ROUND(AVG(PUNT_ACUM),2) AS PROMEDIO_PUNTAJES, REGION 
     FROM (SELECT C.COD_CIA, C.NOMBRE_CIA, P.PUNT_ACUM, J.REGION, J.TIPO_COL, J.NOMBRE_COL
@@ -85,3 +134,27 @@ SELECT ROUND(AVG(PUNT_ACUM),2) AS PROMEDIO_PUNTAJES, REGION
     LEFT JOIN (SELECT COL.REGION, COL.TIPO_COL, COL.NOMBRE_COL, E.COD_CIA
     FROM ESTUDIANTE E LEFT JOIN COLEGIO COL ON COL.COD_COLEGIO = E.COD_COLEGIO) J ON J.COD_CIA = C.COD_CIA)
     GROUP BY REGION
+
+-- Para la informacion del jurado
+SELECT COD_CIA, NOMBRE_CIA, COL.NOMBRE_COL FROM COMPAÑIA C, COLEGIO COL 
+       WHERE COL.COD_COLEGIO = C.COLEGIO_PROC AND Fase_alcanzada IN (SELECT MAX(Fase_alcanzada) FROM COMPAÑIA)
+
+-- Mostrar resultados.
+SELECT COD_CIA, NOMBRE_CIA, COL.NOMBRE_COL, FASE_ALCANZADA FROM COMPAÑIA C, COLEGIO COL
+            WHERE COL.COD_COLEGIO = C.COLEGIO_PROC ORDER BY C.FASE_ALCANZADA DESC
+
+-- Mostrar mantenimiento. Join de 3 tablas
+SELECT C.COD_CIA, C.LOGO, C.CDESCRIPCION, C.NOMBRE_CIA, C.COLEGIO_PROC,
+        C.FASE_ALCANZADA, RS.LINK, RS.RSNOMBRE AS NOMBRE_RED_SOCIAL, P.PUNT_ACUM, P.COD_FASE FROM COMPAÑIA C
+LEFT JOIN RED_SOCIAL RS ON C.COD_CIA = RS.COD_CIA 
+LEFT JOIN PARTICIPA_EN P ON P.COD_CIA = C.COD_CIA
+
+--SELECT DE MANTENIMIENTO DE JURADOS.
+SELECT COD_JURADO, JPRIMER_NOMBRE, JPRIMER_APELLIDO, CORREO_JURADO, LINKEDIN FROM JURADO
+
+--SELECT PARA ACTIVIDADES DE JURADO
+--SELECT * FROM ACTIVIDAD WHERE COD_JURADO = {}
+
+--SELECT PARA LA VISTA DEL ADMIN. SIGUIENTE FASE.
+SELECT COD_CIA, NOMBRE_CIA, COL.NOMBRE_COL,
+        FASE_ALCANZADA FROM COMPAÑIA C, COLEGIO COL WHERE COL.COD_COLEGIO = C.COLEGIO_PROC ORDER BY C.FASE_ALCANZADA DESC
